164e943c444c7767c307478f7bc491640e056029
FlorianSuch <florian.such@efrei.net>
2026-03-11 14:39:03 +0100

    ajout 4_insertion (seulement Test et Drop)

---
+++ 4_interrogation.sql
@@ -0,0 +1,23 @@
+use bd_projet_s4;
+
+-- Test
+Select * from athletes;
+Select * from categorie;
+Select * from classement;
+Select * from clubs;
+Select * from combats;
+Select * from competitions;
+Select * from inscription;
+Select * from saisons;
+
+-- Suppression
+DROP TABLE IF EXISTS Inscription;
+DROP TABLE IF EXISTS Classement;
+DROP TABLE IF EXISTS Combats;
+DROP TABLE IF EXISTS Competitions;
+DROP TABLE IF EXISTS Athletes;
+DROP TABLE IF EXISTS Categorie;
+DROP TABLE IF EXISTS Saisons;
+DROP TABLE IF EXISTS Clubs;
+
+-- Interrogation
